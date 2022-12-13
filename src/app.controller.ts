0038a373583e875388eb5bca211f6728ba082286
Henrique Cechinel <henrique-cechinel@hotmail.com>
2022-12-13 15:15:57 -0300

    initializing project

---
+++ src/app.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Post, Get, Body } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto'; 
+import { CreateNotificationBody } from './create-notification-body';
+
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notifications.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+
+    const { content, category, recipientId } = body; 
+
+    await this.prisma.notifications.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId
+      }
+    })
+  }
+}
