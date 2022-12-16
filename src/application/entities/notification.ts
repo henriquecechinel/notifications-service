9163229e812b4db627483c03d19dc188cc4c6e4e
Henrique Cechinel <henrique-cechinel@hotmail.com>
2022-12-16 00:32:17 -0300

    added new column 'canceledAt'

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -7,6 +7,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -56,6 +57,14 @@ export class Notification {
 
   public get readAt(): Date | undefined | null {
     return this.props.readAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+
+  public get canceledAt(): Date | undefined | null {
+    return this.props.canceledAt;
   }
 
   public get createdAt(): Date {
